2ce6510c2a3029752aee6c5408ca5db951684290
arsuhinars <arsuhinars@gmail.com>
2023-02-05 20:13:49 +0400

    ✨ Added schemas module

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,7 @@
+from .user_schema import UserSchema
+from .user_create_schema import UserCreateSchema
+from .article_schema import ArticleSchema
+from .article_blocks import (
+    ArticleHeader, ArticleParagraph, ArticleQuote, ArticleList,
+    ArticleHorizontalRule, ArticleImage, ArticleBlock
+)
