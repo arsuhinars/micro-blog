4200f8def55e9fb6ec880903f1d319fe00d537bd
arsuhinars <arsuhinars@gmail.com>
2023-02-05 20:11:57 +0400

    ✨ Added settings module

---
+++ backend/app/settings.py
@@ -0,0 +1,10 @@
+from pydantic import BaseSettings
+
+
+class AppSettings(BaseSettings):
+    database_url: str
+    redis_url: str
+    secret_key: str
+
+    class Config:
+        secrets_dir = '/run/secrets'
