7657448a85538828d99fe392631b361287cfd610
arsuhinars <arsuhinars@gmail.com>
2023-02-05 20:11:39 +0400

    ✨ Added redis module

---
+++ backend/app/redis.py
@@ -0,0 +1,20 @@
+from contextlib import AbstractAsyncContextManager, asynccontextmanager
+
+from redis.asyncio import ConnectionPool, Redis
+
+
+class RedisDatabase:
+    def __init__(self, url: str):
+        self._conn_poll = ConnectionPool.from_url(url)
+
+    @property
+    def connection_pool(self):
+        return self._conn_poll
+
+    @asynccontextmanager
+    async def client(self) -> callable[..., AbstractAsyncContextManager[Redis]]:
+        try:
+            client = Redis(connection_pool=self._conn_poll)
+            yield client
+        finally:
+            await client.close()
