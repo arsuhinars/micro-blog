091054942d55d776b7817d87ea6a7ad59b651632
arsuhinars <arsuhinars@gmail.com>
2023-02-07 20:39:57 +0400

    ✨ Added FastAPI dependency for access token validation

---
+++ backend/app/dependencies.py
@@ -0,0 +1,23 @@
+from fastapi import Depends
+from dependency_injector.wiring import inject, Provide
+
+from app.container import AppContainer
+from app.services import AuthService
+from app.exceptions import AuthorizationRequiredError
+
+
+@inject
+def validate_access_token(
+    access_token: str | None = None,
+    auth_service: AuthService = Depends(Provide[AppContainer.auth_service])
+) -> int:
+    """
+        Validate `access_token` provided by client in query parameters. Can be
+        used as the FastAPI dependency.
+
+        Returns:
+            User id from decoded token
+    """
+    if access_token is None:
+        raise AuthorizationRequiredError()
+    return auth_service.validate_access_token(access_token)
