d8a5a30ccf811e35d874002429b04f4e9cc53b9a
arsuhinars <arsuhinars@gmail.com>
2023-02-07 20:32:57 +0400

    ✨ Added new schemas

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,7 +1,9 @@
 from .user_schema import UserSchema
 from .user_create_schema import UserCreateSchema
+from .auth_tokens import AuthTokens
 from .article_schema import ArticleSchema
 from .article_blocks import (
     ArticleHeader, ArticleParagraph, ArticleQuote, ArticleList,
     ArticleHorizontalRule, ArticleImage, ArticleBlock
 )
+from .error_response import ErrorResponse
