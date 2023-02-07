cb11d84983d85acf6ec7beb5ff80b75971f185ef
arsuhinars <arsuhinars@gmail.com>
2023-02-07 20:42:32 +0400

    ✨ Added exception handlers into FastAPI app

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError, HTTPException
 
 from app.container import AppContainer
 from app.endpoints.user import router as user_router
 from app.endpoints.article import router as article_router
 from app.endpoints.auth import router as auth_router
+from app.exceptions import AppException
+from app.error_handlers import (
+    handle_app_exception, handle_validation_error, handle_http_exception
+)
 
 
 def create_app() -> FastAPI:
@@ -14,6 +19,11 @@ def create_app() -> FastAPI:
     app.include_router(user_router)
     app.include_router(article_router)
     app.include_router(auth_router)
+
+    app.add_exception_handler(AppException, handle_app_exception)
+    app.add_exception_handler(RequestValidationError, handle_validation_error)
+    app.add_exception_handler(HTTPException, handle_http_exception)
+    
     return app
 
 
