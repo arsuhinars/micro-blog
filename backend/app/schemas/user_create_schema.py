1d922b35225e41abb741a99003c67d5e82a46a87
arsuhinars <arsuhinars@gmail.com>
2023-02-11 22:48:51 +0400

    🦺 Added more constrains to user related schemas

--- backend/app/schemas/user_create_schema.py
+++ backend/app/schemas/user_create_schema.py
@@ -1,7 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, constr
+
+import app.config as config
 
 
 class UserCreateSchema(BaseModel):
-    email: str
-    password: str
-    display_name: str
+    email: constr(max_length=config.USER_EMAIL_LENGTH)
+    password: constr(
+        min_length=config.USER_PASSWORD_MIN_LENGTH,
+        max_length=config.USER_PASSWORD_MAX_LENGTH
+    )
+    display_name: constr(
+        strip_whitespace=True,
+        min_length=1,
+        max_length=config.USER_DISPLAY_NAME_LENGTH
+    )
--- backend/app/schemas/user_schema.py
+++ backend/app/schemas/user_schema.py
@@ -1,13 +1,19 @@
 from datetime import date
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, constr
+
+import app.config as config
 
 
 class UserSchema(BaseModel):
     id: int | None
     email: EmailStr | None
     creation_date: date | None
-    display_name: str
+    display_name: constr(
+        strip_whitespace=True,
+        min_length=1,
+        max_length=config.USER_DISPLAY_NAME_LENGTH
+    )
 
     class Config:
         orm_mode = True
