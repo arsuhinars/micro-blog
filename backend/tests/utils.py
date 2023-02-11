f70afc6f589d2a7bd3c35e1785d512ac26e43890
arsuhinars <arsuhinars@gmail.com>
2023-02-11 22:49:45 +0400

    🐛 Fixed type annotation in test utils

--- backend/tests/utils.py
+++ backend/tests/utils.py
@@ -1,10 +1,9 @@
 from httpx import Response
 
 from app.schemas import ErrorResponse
-
 from app.exceptions import AppException
 
 
-def assert_app_error(response: Response, exc: AppException):
+def assert_app_error(response: Response, exc: type[AppException]):
     assert response.status_code == exc.status_code
     assert ErrorResponse.parse_obj(response.json()).error_code == exc.error_code
