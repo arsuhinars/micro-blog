f6b871d4cfc386ad42c50edc3f1554a879e0ef02
arsuhinars <arsuhinars@gmail.com>
2023-02-14 23:58:52 +0400

    🎨 Added new constrains into article blocks schemas

--- backend/app/schemas/article_blocks.py
+++ backend/app/schemas/article_blocks.py
@@ -1,28 +1,30 @@
 from typing import Literal
 
-from pydantic import BaseModel, HttpUrl, conint
+from pydantic import BaseModel, HttpUrl, conint, constr
+
+import app.config as config
 
 
 class ArticleHeader(BaseModel):
     type: Literal["header"]
     heading_level: conint(ge=1, le=6)
-    content: str
+    content: constr(max_length=config.ARTICLE_HEADER_LENGTH)
 
 
 class ArticleParagraph(BaseModel):
     type: Literal["paragraph"]
-    content: str
+    content: constr(max_length=config.ARTICLE_PARAGRAPH_LENGTH)
 
 
 class ArticleQuote(BaseModel):
     type: Literal["quote"]
-    content: str
+    content: constr(max_length=config.ARTICLE_QUOTE_LENGTH)
 
 
 class ArticleList(BaseModel):
     type: Literal["list"]
     list_type: Literal["ordered", "unordered"]
-    content: list[str]
+    content: list[constr(max_length=config.ARTICLE_LIST_ITEM_LENGTH)]
 
 
 class ArticleHorizontalRule(BaseModel):
@@ -41,5 +43,5 @@ ArticleBlock = (
     | ArticleQuote
     | ArticleList
     | ArticleHorizontalRule
-    | ArticleImage
+    # | ArticleImage
 )
