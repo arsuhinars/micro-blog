c2647e8d654f66cc51e96db42df7501b77e86fa7
arsuhinars <arsuhinars@gmail.com>
2023-02-14 23:58:33 +0400

    🎨 Added new fields and contrains into ArticleSchema

--- backend/app/schemas/article_schema.py
+++ backend/app/schemas/article_schema.py
@@ -11,9 +11,11 @@ class ArticleSchema(BaseModel):
     author_id: int | None
     creation_time: datetime | None
     update_time: datetime | None
-    title: constr(max_length=config.ARTICLE_TITLE_LENGTH)
+    title: constr(min_length=1, max_length=config.ARTICLE_TITLE_LENGTH)
     body: list[ArticleBlock]
     is_published: bool
+    views_count: int | None
+    likes_count: int | None
 
     class Config:
         orm_mode = True
