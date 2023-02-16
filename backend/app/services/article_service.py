2bfbddebbfd5dd3436f11e55ddafa22f60115f5b
arsuhinars <arsuhinars@gmail.com>
2023-02-16 23:48:38 +0400

    ✏️ Added self in ArticleService methods

--- backend/app/services/article_service.py
+++ backend/app/services/article_service.py
@@ -6,14 +6,14 @@ class ArticleService:
     def __init__(self, article_repo: ArticleRepository):
         self._article_repo = article_repo
 
-    async def create(title: str, author_id: int) -> ArticleSchema:
+    async def create(self, title: str, author_id: int) -> ArticleSchema:
         ...
 
-    async def get_by_id(id: int) -> ArticleSchema | None:
+    async def get_by_id(self, id: int) -> ArticleSchema | None:
         ...
 
-    async def get_by_author_id(author_id: int) -> list[ArticleSchema]:
+    async def get_by_author_id(self, author_id: int) -> list[ArticleSchema]:
         ...
 
-    async def update(article: ArticleSchema) -> ArticleSchema:
+    async def update(self, article: ArticleSchema) -> ArticleSchema:
         ...
