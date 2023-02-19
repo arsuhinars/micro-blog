52823ca68111c2c433d8352aefbb19f08cc48021
arsuhinars <arsuhinars@gmail.com>
2023-02-19 16:10:58 +0400

    ✨ Implemented article endpoint

--- backend/app/endpoints/article.py
+++ backend/app/endpoints/article.py
@@ -2,11 +2,12 @@ from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Depends
 
 from app.container import AppContainer
-from app.dependencies import validate_access_token
-from app.schemas import ArticleSchema
+from app.dependencies import validate_access_token, validate_optional_access_token
+from app.exceptions import AccessDeniedError
+from app.schemas import ArticleCreateSchema, ArticleSchema
 from app.services import ArticleService
 
-router = APIRouter()
+router = APIRouter(tags=["Article"])
 
 
 @router.post(
@@ -15,12 +16,12 @@ router = APIRouter()
     summary="Creates new user article",
 )
 @inject
-def create_article(
-    article: ArticleSchema,
+async def create_article(
+    article_create_data: ArticleCreateSchema,
     article_service: ArticleService = Depends(Provide[AppContainer.article_service]),
     user_id: int = Depends(validate_access_token),
 ):
-    ...
+    return await article_service.create(article_create_data.title, user_id)
 
 
 @router.put(
@@ -31,13 +32,18 @@ def create_article(
     private and user is not its author""",
 )
 @inject
-def update_article(
+async def update_article(
     article_id: int,
     article: ArticleSchema,
     article_service: ArticleService = Depends(Provide[AppContainer.article_service]),
     user_id: int = Depends(validate_access_token),
 ):
-    ...
+    db_article = await article_service.get_by_id(article_id)
+    if db_article.author_id != user_id:
+        raise AccessDeniedError()
+
+    article.id = article_id
+    return await article_service.update(article)
 
 
 @router.get(
@@ -47,12 +53,16 @@ def update_article(
     description="""Raise 403 status code (Forbidden) if article exists, but is
     private and user is not its author""",
 )
-def get_article_by_id(
+async def get_article_by_id(
     article_id: int,
     article_service: ArticleService = Depends(Provide[AppContainer.article_service]),
-    user_id: int = Depends(validate_access_token),
+    user_id: int | None = Depends(validate_optional_access_token),
 ):
-    ...
+    article = await article_service.get_by_id(article_id)
+    if not article.is_published and user_id != article.author_id:
+        raise AccessDeniedError()
+
+    return article
 
 
 @router.get(
@@ -62,9 +72,12 @@ def get_article_by_id(
     description="""Returns only available articles, e.g if it is requested by other
     users, then only public articles are returned.""",
 )
-def get_user_articles(
+async def get_user_articles(
     user_id: int,
     article_service: ArticleService = Depends(Provide[AppContainer.article_service]),
-    curr_user_id: int = Depends(validate_access_token),
+    curr_user_id: int | None = Depends(validate_optional_access_token),
 ):
-    ...
+    articles = await article_service.get_by_author_id(user_id)
+    if curr_user_id != user_id:
+        articles = [a for a in articles if a.is_published]
+    return list(map(lambda a: a.id, articles))
